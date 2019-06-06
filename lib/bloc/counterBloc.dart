6bf966e8008d80a9d4ac40f7b9801d2769455437
Mitul Gautam <mitulgautam@outlook.com>
2019-06-06 23:01:20 +0530

    Completed

--- lib/bloc/counterBloc.dart
+++ lib/bloc/counterBloc.dart
@@ -1,1 +1,9 @@
-class CounterBloc {}
+import 'dart:async';
+
+class CounterBloc {
+  StreamController<int> _streamController = StreamController<int>();
+  StreamSink<int> get inputSink => _streamController.sink;
+  Stream<int> get outputStream => _streamController.stream;
+
+  CounterBloc() {}
+}
--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,3 +1,5 @@
+import 'package:counter_app_bloc/bloc/counterBloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -6,8 +8,61 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int counter = 0;
+  CounterBloc bloc = CounterBloc();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          StreamBuilder<int>(
+              initialData: 0,
+              stream: bloc.outputStream,
+              builder: (context, snapshot) {
+                return Text(
+                  snapshot.data.toString(),
+                  style: TextStyle(color: Colors.pink, fontSize: 192.0),
+                );
+              }),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              OutlineButton(
+                padding: EdgeInsets.all(32.0),
+                onPressed: () {
+                  bloc.inputSink.add(--counter);
+                },
+                color: Colors.pink,
+                splashColor: Colors.pinkAccent,
+                highlightedBorderColor: Colors.pink,
+                child: Icon(
+                  Icons.exposure_neg_1,
+                  size: 64.0,
+                  color: Colors.pink,
+                ),
+              ),
+              RaisedButton(
+                padding: EdgeInsets.all(32.0),
+                onPressed: () {
+                  bloc.inputSink.add(++counter);
+                },
+                color: Colors.pink,
+                splashColor: Colors.white,
+                child: Icon(
+                  Icons.exposure_plus_1,
+                  size: 64.0,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
